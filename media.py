f702efb6866631dec900db9dacc93e3014646de9
lightfootc <34262488+lightfootc@users.noreply.github.com>
2018-02-11 21:07:31 -0600

    Update media.py
    Took away the whitespace and change some indentation

--- media.py
+++ media.py
@@ -7,10 +7,11 @@ __init__ method inside the class. This __init__ method initializes all
 of the data inside the class. The constuctor uses the key word self.
 You can think of the key word self as itself of the instances in question.
 So if an instance of Avatar is being created, Avatar is self.'''
+
+
 class Movie():
     '''This class a way to store my favorite movies and related information'''
-    VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']
-    
+    VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']    
     '''The variables associated with a specific instance is called instance
     variables. These are unique to an object. They can be access by using the
     self keyword inside the class and the instance name outside the class.'''
@@ -23,7 +24,6 @@ class Movie():
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-        
     '''This function inside of the Movie class that has the first argument
     as self is called instance method.'''
     def show_trailer(self):
